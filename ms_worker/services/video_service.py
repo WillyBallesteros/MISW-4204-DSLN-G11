0f3e3e1f5cf89481046990623b3f2093778b4003
Luis Daniel Angel <l.angelb@uniandes.edu.co>
2023-10-19 20:08:53 -0500

    Add codec worker

--- ms_worker/services/video_service.py
+++ ms_worker/services/video_service.py
@@ -3,10 +3,8 @@ import moviepy.editor as mp
 def convert_video(input, output, codec):
   try:
     video_clip = mp.VideoFileClip(input)
-    video_clip.write_videofile(output)
+    video_clip.write_videofile(output, codec=codec)
     video_clip.close()
     return None
   except Exception as e:
     return e
-
-
