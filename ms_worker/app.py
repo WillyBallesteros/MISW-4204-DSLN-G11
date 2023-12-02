41a0b601ad2bb2da19ccbcde7aa3490711b7d00e
Nicolás Hüg <r.hug@uniandes.edu.co>
2023-12-02 11:23:28 -0300

    Nuevo health check para el worker

--- ms_worker/app.py
+++ ms_worker/app.py
@@ -1,9 +1,14 @@
 from services import queue_service, TASKS_TOPIC
+from flask_restful import Api
 from flask import Flask
 import os
 import threading
+from views.views import ViewHealth
 
 app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(ViewHealth, '/api/health')
 
 def run_consumer():
     queue_service.listener_queue(TASKS_TOPIC)
